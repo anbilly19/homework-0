714ee3d1f30dfdbc8757febf1ae18e798ff11b1a
thadodga <lowrider666@web.de>
2018-07-11 11:04:44 +0200

    Update VERSION_1.R

--- VERSION_1.R
+++ VERSION_1.R
@@ -12,14 +12,28 @@ hb_mod<-hb_mod%>%select(high,low,Counted.Homeownership.Units,Counted.Rental.Unit
 
 #setting new variable All.Units
 hb_mod$All.Units<-hb_mod$Counted.Homeownership.Units+hb_mod$Counted.Rental.Units
-#Percent low 
+#percentage low 
 hb_mod$Percent.of.low.income.Units<-(hb_mod$low/hb_mod$All.Units)*100
-#Percent high
+#percentage high
 hb_mod$Percent.of.high.income.Units<-(hb_mod$high/hb_mod$All.Units)*100
-#Percent of rental
+#percentage of rental
 hb_mod$Percent.of.rental.Units<-(hb_mod$Counted.Rental.Units/hb_mod$All.Units)*100
-#Percent of homeownership
+#percentage of homeownership
 hb_mod$Percent.of.homeownership.Units<-(hb_mod$Counted.Homeownership.Units/hb_mod$All.Units)*100
 
 hb_mod
 
+#Next thing is regression. We want to predict our Dependent Variable 'Counted Homeownership Units' per House.
+#We have 'low', 'high', 'TotalNumberOfHousingUnits' and our percentage of ____ per House as Independent Variables.
+#I would say we take 100 cases and delete the Values of 'Counted Homeownership Units'.
+#Then we make a Regression Analysis with the rest and predict the 100 cases. We want to reach 95 right cases.
+
+#Take 100
+the100 <- tail(hb_mod, 100)
+
+#make a new column names Predicted.Homeownership.Units, empty
+mutate(the100, 'Predicted.Homeownership.Units' = NA)
+
+#Linear Regression
+linearMod <- lm(hb_mod$Counted.Homeownership.Units ~ hb_mod$high + hb_mod$low + hb_mod$All.Units, data=hb_mod)  # build linear regression model on full data
+print(linearMod)
